7e414429aafa6f6a151617c75183e0c8a06a7858
bold ariunbold <ariunboldbold200@gmail.com>
2023-11-05 19:39:27 +0800

    commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <body>
+      <title>bekua</title>
+      <div className="ytplayer">
+        <iframe
+          width="640"
+          height="360"
+          src="https://hentaibar.com/embed/650"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </body>
+  );
+}
